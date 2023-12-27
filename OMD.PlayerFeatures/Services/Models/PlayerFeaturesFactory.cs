76cbc5b80fb87d681d2b9e3451a26f3b0514e20d
DevInc0 <defaultdev0x101@gmail.com>
2023-12-27 14:44:47 +0300

    feat: changed features factory body

--- OMD.PlayerFeatures/Services/Models/PlayerFeaturesFactory.cs
+++ OMD.PlayerFeatures/Services/Models/PlayerFeaturesFactory.cs
@@ -3,6 +3,7 @@ using OMD.PlayersFeatures.Enumerations;
 using OMD.PlayersFeatures.Models;
 using OpenMod.API.Ioc;
 using OpenMod.Unturned.Players;
+using SDG.Unturned;
 using System;
 
 namespace OMD.PlayersFeatures.Services;
@@ -10,21 +11,25 @@ namespace OMD.PlayersFeatures.Services;
 [PluginServiceImplementation(Lifetime = ServiceLifetime.Singleton)]
 public sealed class PlayerFeaturesFactory : IPlayerFeaturesFactory
 {
-    private FeaturesIntegrationType _type;
+    private Func<Player, PlayerFeatures> _featuresFactory = null!;
 
     public void SetIntegrationType(string type)
     {
-        if (!Enum.TryParse(type, true, out _type))
-        {
-            _type = FeaturesIntegrationType.None;
-
+        if (!Enum.TryParse(type, true, out FeaturesIntegrationType integrationType))
             throw new ArgumentException($"Failed to parse \"{type}\" to {nameof(FeaturesIntegrationType)}");
-        }
+
+        _featuresFactory = integrationType switch {
+            FeaturesIntegrationType.RocketMod => (player) => new RocketModPlayerFeatures(player),
+            FeaturesIntegrationType.OpenMod => (player) => new OpenModPlayerFeatures(player),
+            _ => (player) => throw new InvalidOperationException("Features integration type is not set!")
+        };
     }
 
-    public PlayerFeatures CreateFor(UnturnedPlayer player) => _type switch {
-        FeaturesIntegrationType.RocketMod => new RocketModPlayerFeatures(player.Player),
-        FeaturesIntegrationType.OpenMod => new OpenModPlayerFeatures(player.Player),
-        _ => throw new ArgumentException("Features integration type is not set!"),
-    };
+    public PlayerFeatures CreateFor(UnturnedPlayer player)
+    {
+        if (_featuresFactory is null)
+            throw new InvalidOperationException("Features integration type is not set!");
+
+        return _featuresFactory(player.Player);
+    }
 }
